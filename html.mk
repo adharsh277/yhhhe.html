f5f34e27efe3cd9a5b2250b16291db4909dc5933
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2022-11-12 20:54:24 +0530

    Create html.mk

---
+++ html.mk
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>LIONEL MESSI - ALIEN OR HUMAN</h1>
+    <p>
+      So coming to the news report of daily hunt of 2078 , the peoples are
+      saying that leo could be a alien from another university but accrding to
+      the daily report of telecast interviewr saying that, he is a purely man
+      with sexuall feeling not alien
+    </p>
+    <h2>WHY RONALDO FEAR OF LIONEL MESSI</h2>
+    <p>
+      In that case leos diehard fans arre saying that rono is a selfishh funda
+      with not a character , just he does only chutiya things
+    </p>
+    <h3>WHO IS GOAT? LEO OR CR</h3>
+    <p>
+      People have so manyy opinion about that according to me both are extra
+      ordinary players , we have to enjoy thier playing style , and dont make
+      any any scenes because just love the game guys.Both people have thier oen
+      ability to do any work.
+    </p>
+    <h4>SIGINING OF ADHARSH</h4>
+
+    <text-align: center></text-align:>
+    <p>
+      SO ENJOY YOUR LIFE BY FULL OF HAPPINES AND BE THE SUPPORTER OF YOUR TEAM
+      AND LETS CHEER THEM
+    </p>
+    <h1>HERE WE GO,,</h1>
+    <a
+      href="https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022"
+      >This is a link</a
+    >
+  </body>
+</html>
